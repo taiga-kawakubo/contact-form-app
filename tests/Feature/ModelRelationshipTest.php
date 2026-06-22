4c064a6a7b11d73c8ec7cc10940469f3cdcde0fe
taiga kawakubo <kawakubo.t.14@gmail.com>
2026-06-22 10:27:18 +0900

    Implement seeders and factory for sample data

--- tests/Feature/ModelRelationshipTest.php
+++ tests/Feature/ModelRelationshipTest.php
@@ -6,9 +6,7 @@ use App\Models\Category;
 use App\Models\Contact;
 use App\Models\Tag;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
-
 
 
 
